42789ae053f7dc8a4e2328a9aadb380652a200e1
vxuandai0212 <vxuandai@gmail.com>
2018-04-19 16:19:52 +0700

    Add newMessage and createMessage

--- server/server.js
+++ server/server.js
@@ -13,6 +13,17 @@ app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
     console.log('New user connected');
+
+    socket.emit('newMessage', {
+        from: 'Anna',
+        text: 'Hey. You are so nice',
+        createAt: 123
+    });
+
+    socket.on('createMessage', (message) => {
+        console.log('createMessage', message);
+    });
+
     socket.on('disconnect', () => {
         console.log('User was disconnected');
     });
