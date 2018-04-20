9b1f8e61a7e50a2b6fd43842b311aeeadf396946
vxuandai0212 <vxuandai@gmail.com>
2018-04-20 10:03:32 +0700

    send message to all server not only 1 socket

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ io.on('connection', (socket) => {
     socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
 
     socket.on('createMessage', (message) => {
-        socket.emit('newMessage', generateMessage(message.from, message.text));
+        io.emit('newMessage', generateMessage(message.from, message.text));
     });
 
     socket.on('disconnect', () => {
