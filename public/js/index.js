15a06893241fd05f4ec7a823d9c9cdbc72318475
vxuandai0212 <vxuandai@gmail.com>
2018-04-26 09:02:00 +0700

    Add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -33,7 +33,7 @@ jQuery('#message-form').on('submit', function (e) {
         from: 'User',
         text: jQuery('[name=message]').val()
     }, function () {
-
+        jQuery('[name=message]').val('');
     });
 });
 
@@ -43,12 +43,16 @@ locationButton.on('click', function () {
         return alert('Geolocation not supported by your browser.');
     }
 
+    locationButton.attr('disabled', 'disabled').text('Sending location...');
+
     navigator.geolocation.getCurrentPosition(function (position) {
+        locationButton.removeAttr('disabled').text('Send Location');
         socket.emit('createLocationMessage', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         });
     }, function () {
+        locationButton.removeAttr('disabled').text('Send Location');
         alert('Unable to fetch location.');
     });
 });
--- server/server.js
+++ server/server.js
@@ -19,8 +19,9 @@ io.on('connection', (socket) => {
 
     socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
 
-    socket.on('createMessage', (message) => {
+    socket.on('createMessage', (message, callback) => {
         io.emit('newMessage', generateMessage(message.from, message.text));
+        callback();
     });
 
     socket.on('createLocationMessage', (coords) => {
