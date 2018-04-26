54ff762cbfe0dc0790184f06e762d36d75aced49
vxuandai0212 <vxuandai@gmail.com>
2018-04-26 15:33:48 +0700

    Add Users class and updateUserList event

--- public/js/chat.js
+++ public/js/chat.js
@@ -16,11 +16,29 @@ function scrollToBottom () {
 }
 
 socket.on('connect', function () {
-    console.log('Connected to server');
+    var params = jQuery.deparam(window.location.search);
+
+    socket.emit('join', params, function (err) {
+        if (err) {
+            alert(err);
+            window.history.back();
+        } else {
+
+        }
+    });
 });
 
 socket.on('disconnect', function () {
     console.log('Disconnected from server');
+});
+
+socket.on('updateUserList', function (users) {
+    var ol = jQuery('<ol></ol>');
+    users.forEach(function (user) {
+        ol.append(jQuery('<li></li>').text(user));
+    });
+
+    jQuery('#users').html(ol);
 });
 
 socket.on('newMessage', function (message) {
